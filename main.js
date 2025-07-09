b4fae932a1779fb1d34c95cba943814724c68c46
dtsiomapdx <dev.dtsioma@gmail.com>
2025-07-09 11:38:15 -0700

    added vim left/right

--- main.js
+++ main.js
@@ -2,12 +2,44 @@ let vimMode = false;
 let activeCardIndex = 0;
 
 window.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-        vimMode = !vimMode;
-        if (vimMode) {
-            document.querySelector(".card").classList.add("active");
-        } else {
-            document.querySelector(".card").classList.remove("active");
-        }
+    switch (event.key) {
+        case "Escape":
+            toggleVimMode();
+            break;
+        case "h":
+            vimMode && navigateLeft();
+            break;
+        case "l":
+            vimMode && navigateRight();
+            break;
     }
 });
+
+function toggleVimMode() {
+    vimMode = !vimMode;
+    activeCardIndex = 0;
+    if (vimMode) {
+        document.querySelector(".card").classList.add("active");
+    } else {
+        document.querySelector(".card.active").classList.remove("active");
+    }
+}
+
+function navigateLeft() {
+    if (activeCardIndex > 0) {
+        const currentActive = document.querySelector(".card.active");
+        currentActive.previousElementSibling.classList.add("active");
+        currentActive.classList.remove("active");
+        activeCardIndex--;
+    }
+}
+
+function navigateRight() {
+    const numberOfCards = document.querySelectorAll(".card").length;
+    if (activeCardIndex < numberOfCards - 1) {
+        const currentActive = document.querySelector(".card.active");
+        currentActive.nextElementSibling.classList.add("active");
+        currentActive.classList.remove("active");
+        activeCardIndex++;
+    }
+}
